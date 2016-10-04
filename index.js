3abdc0d88348940d510474241184bbc1067ce4f2
Jaylan <Jaylan.Tse@gmail.com>
2016-10-04 22:56:46 +0000

    part2

--- index.js
+++ index.js
@@ -4,12 +4,37 @@ var fs = require('fs');
 var PORT = (process.env.PORT || 8080); 
 
 http.createServer((req, res) => {	
-	fs.readFile('public/index.html', (err, data) => {
-		if (err) throw err;
-		res.writeHead(200, {"Content-Type": "text/html"});
-		res.write(data.toString());
-		res.end();
+	var index = 'public/index.html';
+
+	fs.stat(index, (err, stats) => {
+		if (err && err.code == 'ENOENT') {
+			res.writeHead(400, {"Content-Type": "text/plain"});
+			res.write("404: NOT FOUND");
+			res.end();
+			return;
+		} else if (err) {
+			throw err;
+		}
+
+		fs.open(index, 'r', (err, fd) => {
+			if (err) throw err;
+
+			var	buffer = new Buffer(stats.size);
+
+			fs.read(fd, buffer, 0, buffer.length, null, (err, bytesRead, buffer) => {
+				if (err) {
+					fs.close(fd);
+					throw err;
+				}
+
+				res.writeHead(200, {"Content-Type": "text/html"});
+				res.write(buffer.toString('utf8', 0, buffer.length));
+				fs.close(fd);
+				res.end();
+			});
+		});
 	});
+
 }).listen(PORT, () => {
     console.log("Server listening on: http://localhost:%s", PORT);
 });
