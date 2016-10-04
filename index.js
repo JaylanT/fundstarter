a8eb230b0882a7d4b30ecdcfcd55d298b121a2bd
Jaylan <Jaylan.Tse@gmail.com>
2016-10-04 22:55:45 +0000

    part1b.js

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ var fs = require('fs');
 var PORT = (process.env.PORT || 8080); 
 
 http.createServer((req, res) => {	
-	var index = fs.readFileSync('public/index.html', 'utf8');
-
-	res.writeHead(200, {"Content-Type": "text/html"});
-	res.write(index);
-	res.end();
+	fs.readFile('public/index.html', (err, data) => {
+		if (err) throw err;
+		res.writeHead(200, {"Content-Type": "text/html"});
+		res.write(data.toString());
+		res.end();
+	});
 }).listen(PORT, () => {
     console.log("Server listening on: http://localhost:%s", PORT);
 });
